f68517309f1ee2d6e9b1f64f5cff4a0520168883
ariqrm <ariqrm@gmail.com>
2024-07-05 02:18:48 +0700

    test group chat with response

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -2,7 +2,7 @@ export const dynamic = 'force-dynamic'
 
 export const fetchCache = 'force-no-store'
 
-import {Bot, webhookCallback} from 'grammy'
+import {Bot, Context, webhookCallback} from 'grammy'
 
 const token = process.env.TELEGRAM_BOT_TOKEN
 
@@ -10,23 +10,36 @@ if (!token)
   throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
 const bot = new Bot(token)
-const sendMsgToGroup = async (msg: string) => {
+const sendMsgToGroup = async (ctx: Context, msg: string) => {
   const splitmsg = msg?.split(',')
   const groupId = splitmsg
-    .find(fn => fn.match(/\^ID:/))
+    .find(fn => fn.match(/\^ID:-1002249803972/))
     ?.replace('ID:', '')
     .trim()
   const newmsg = splitmsg
     .find(fn => fn.match(/\^MSG:/))
     ?.replace('MSG:', '')
     .trim()
+  if (groupId) {
+    await ctx.reply('Group Id : ' + groupId)
+  }
+  if (newmsg) {
+    await ctx.reply('message : ' + newmsg)
+  }
   if (groupId && newmsg) {
-    await bot.api.sendMessage(groupId, newmsg)
+    await bot.api
+      .sendMessage(groupId, newmsg)
+      .then(async th => {
+        await ctx.reply('message th : ' + JSON.stringify({th}))
+      })
+      .catch(async ch => {
+        await ctx.reply('message ch : ' + JSON.stringify({ch}))
+      })
   }
 }
 bot.on('message:text', async ctx => {
   if (ctx.message.text) {
-    await sendMsgToGroup(ctx.message.text)
+    await sendMsgToGroup(ctx, ctx.message.text)
     await ctx.reply('You Send It : ' + ctx.message.text)
   } else {
     await ctx.reply(ctx.message.text)
